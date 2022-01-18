5f2d7a1be0e0444413acb209a80c2b15926f2e04
soylent <1593860+soylent@users.noreply.github.com>
2022-01-17 23:16:32 -0700

    Update HLS test

--- tests/streaming/test_hls.py
+++ tests/streaming/test_hls.py
@@ -3,16 +3,16 @@ import offstream.streaming.hls as hls
 
 def test_playlist(m3u8):
     playlist = hls.Playlist(version=4, start_time_offset=1.23, playlist_type="live")
-    playlist.append(url="https://example.org/", duration=4.56)
+    playlist.append(url="https://example.org/", duration=4.5009)
     playlist.write(m3u8)
     assert m3u8.read() == (
         "#EXTM3U\n"
         "#EXT-X-VERSION:4\n"
         "#EXT-X-PLAYLIST-TYPE:LIVE\n"
         "#EXT-X-ENDLIST\n"
-        "#EXT-X-TARGETDURATION:4.560\n"
+        "#EXT-X-TARGETDURATION:4.501\n"
         "#EXT-X-START:TIME-OFFSET=1.230\n"
-        "#EXTINF:4.560,\n"
+        "#EXTINF:4.501,\n"
         "https://example.org/\n"
     )
 
