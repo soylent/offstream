56c80138f76269c362ec1c9f52066fb55dfce37e
soylent <1593860+soylent@users.noreply.github.com>
2022-01-19 19:36:07 -0700

    Change playlist type to vod

--- src/offstream/streaming/hls.py
+++ src/offstream/streaming/hls.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import List, NamedTuple, Optional
 
@@ -14,12 +15,10 @@ class Playlist:
     def __init__(
         self,
         version: int = 3,
-        start_time_offset: Optional[float] = 0.0,
-        playlist_type: Optional[str] = "event",
+        playlist_type: Optional[str] = "vod",
     ) -> None:
         self.version = version
         self.playlist_type = playlist_type.upper() if playlist_type else None
-        self.start_time_offset = start_time_offset
         self.segments: List[_Segment] = []
 
     def append(self, url: str, duration: float, title: str = "") -> None:
@@ -27,19 +26,25 @@ class Playlist:
         self.segments.append(segment)
 
     def write(self, path: Path) -> None:
-        target_duration = max(map(lambda s: s.duration, self.segments), default=0)
+        target_duration = max(
+            map(lambda s: math.ceil(s.duration), self.segments), default=10
+        )
         with path.open(mode="w", encoding="utf-8") as m3u8:
             m3u8.write("#EXTM3U\n")
-            m3u8.write(f"#EXT-X-VERSION:{self.version}\n")
             if self.playlist_type is not None:
                 m3u8.write(f"#EXT-X-PLAYLIST-TYPE:{self.playlist_type}\n")
-            # Many players don't support the EXT-X-START tag, so I added
-            # EXT-X-ENDLIST as a workaround. Unfortunately, it prevents players
-            # from reloading the playlist.
+            m3u8.write(f"#EXT-X-TARGETDURATION:{target_duration}\n")
+            m3u8.write(f"#EXT-X-VERSION:{self.version}\n")
+            m3u8.write("#EXT-X-MEDIA-SEQUENCE:0\n")
+            # XXX: Ideally, we want the video player to reload the
+            # playlist periodically:
+            #   #EXT-X-PLAYLIST-TYPE:EVENT
+            #   #EXT-X-START:TIME-OFFSET=0
+            # But FFmpeg doesn't support the EXT-X-START tag, so I had
+            # to change the above to:
+            #   #EXT-X-PLAYLIST-TYPE:VOD
+            #   #EXT-X-ENDLIST
             m3u8.write("#EXT-X-ENDLIST\n")
-            m3u8.write(f"#EXT-X-TARGETDURATION:{target_duration:.3f}\n")
-            if self.start_time_offset is not None:
-                m3u8.write(f"#EXT-X-START:TIME-OFFSET={self.start_time_offset:.3f}\n")
             for segment in self.segments:
                 m3u8.write(f"#EXTINF:{segment.duration:.3f},{segment.title}\n")
                 m3u8.write(f"{segment.url}\n")
--- tests/streaming/test_hls.py
+++ tests/streaming/test_hls.py
@@ -2,17 +2,17 @@ import offstream.streaming.hls as hls
 
 
 def test_playlist(m3u8):
-    playlist = hls.Playlist(version=4, start_time_offset=1.23, playlist_type="live")
-    playlist.append(url="https://example.org/", duration=4.5009)
+    playlist = hls.Playlist(version=4, playlist_type="event")
+    playlist.append(url="https://example.org/", duration=4.1009)
     playlist.write(m3u8)
     assert m3u8.read() == (
         "#EXTM3U\n"
+        "#EXT-X-PLAYLIST-TYPE:EVENT\n"
+        "#EXT-X-TARGETDURATION:5\n"
         "#EXT-X-VERSION:4\n"
-        "#EXT-X-PLAYLIST-TYPE:LIVE\n"
+        "#EXT-X-MEDIA-SEQUENCE:0\n"
         "#EXT-X-ENDLIST\n"
-        "#EXT-X-TARGETDURATION:4.501\n"
-        "#EXT-X-START:TIME-OFFSET=1.230\n"
-        "#EXTINF:4.501,\n"
+        "#EXTINF:4.101,\n"
         "https://example.org/\n"
     )
 
@@ -22,9 +22,9 @@ def test_empty_playlist(m3u8):
     playlist.write(m3u8)
     assert m3u8.read() == (
         "#EXTM3U\n"
+        "#EXT-X-PLAYLIST-TYPE:VOD\n"
+        "#EXT-X-TARGETDURATION:10\n"
         "#EXT-X-VERSION:3\n"
-        "#EXT-X-PLAYLIST-TYPE:EVENT\n"
+        "#EXT-X-MEDIA-SEQUENCE:0\n"
         "#EXT-X-ENDLIST\n"
-        "#EXT-X-TARGETDURATION:0.000\n"
-        "#EXT-X-START:TIME-OFFSET=0.000\n"
     )
